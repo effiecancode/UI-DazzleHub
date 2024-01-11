e0f1b1fa91a123fa3045278226be634251179055
effiecancode <faitheffie25@gmail.com>
2024-01-11 21:44:54 +0300

    up and running

---
+++ 05-loopSlider/slider/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header className="App-header">
+          <h1>#1 Travel Agent</h1>
+          <p>
+          Perhaps travel cannot prevent bigotry, but by demonstrating that all peoples cry, laugh, eat, worry, and die, it can introduce the idea that if we try and understand each other, we may even become friends.
+          </p>
+        </header>
+      </div>
+
+      <div class="slider">
+          <div class="slide-track">
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="300" width="350" alt="" />
+              </div>
+              <div class="slide">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="300" width="350" alt="" />
+              </div>
+          </div>
+    </div>
+
+
+  </div>
+  );
+}
+
+export default App;
