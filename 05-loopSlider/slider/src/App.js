00f66f0e6d7f9fae5d4b0f53efb6e9a21eee05e4
effiecancode <faitheffie25@gmail.com>
2024-01-11 22:37:59 +0300

    layout touches

--- 05-loopSlider/slider/src/App.js
+++ 05-loopSlider/slider/src/App.js
@@ -11,52 +11,58 @@ function App() {
           </p>
         </header>
       </div>
-
       <div class="slider">
           <div class="slide-track">
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/0LwfbRtQ-ac" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/oJofV8dZd_w" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/DWXR-nAbxCk" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/apWEUZdDXdQ" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/8f02hcTVbKs" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/pY4La9JmNvg" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/CG6Gd__QIOY" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/wYy3rvvgjAU" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/1tyuLfDOnG0" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/_pPHgeHz1uk" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/2gOxKj594nM" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/xEaAoizNFV8" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/pRECfI47Q9o" height="300" width="350" alt="" />
               </div>
               <div class="slide">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="300" width="350" alt="" />
+                  <img src="https://source.unsplash.com/DGa0LQ0yDPc" height="300" width="350" alt="" />
               </div>
           </div>
+
+          <div class="info">
+          <p>
+          Perhaps travel cannot prevent bigotry, but by demonstrating that all peoples cry, laugh, eat, worry, and die, it can introduce the idea that if we try and understand each other, we may even become friends.
+          </p>
+      </div>
+
     </div>
 
 
